1357aa836d5df34fff40b5bc967f0b2d9fefc4cc
Bryan Creel <scytalezero@users.noreply.github.com>
2014-12-04 09:38:00 -0500

    Update EasynewsHighlights.user.js

--- EasynewsHighlights.user.js
+++ EasynewsHighlights.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Easynews Highlights
-// @version        2.9.4
+// @version        2.9.5
 // @namespace      http://ligature.me
 // @description    Highlights THUMB, NFO, PAR, NZB, and filenames in global search links.
 // @updateURL      https://github.com/scytalezero/EasynewsHighlights/raw/master/EasynewsHighlights.user.js
@@ -12,70 +12,40 @@
 // @include        https://secure.members.easynews.com/global5/*
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // ==/UserScript==
-/*
-var $;
 
-// Add jQuery
-(function(){
-    if (typeof unsafeWindow.jQuery == 'undefined') {
-        var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
-            GM_JQ = document.createElement('script');
+var thisElement, regex, original;
 
-        GM_JQ.src = '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-        GM_JQ.type = 'text/javascript';
-        GM_JQ.async = true;
+$("a[target^='thumb']").css({"color": "DarkGreen", "background-color": "AliceBlue"});
+$("a[target^='nfo']").css({"color": "DarkRed", "background-color": "AliceBlue"});
+$("a[target^='par']").css({"color": "DarkBlue", "background-color": "AliceBlue"});
+$("a[target^='nzb']").css({"color": "DarkSlateBlue", "background-color": "AliceBlue"});
+$("a[target^='subj']").each(function() {
+var $this = $(this);
+regex = new RegExp("<br>","ig");
+$this.html($this.html().replace(regex, " "));
 
-        GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
-    }
-    GM_wait();
-})();
-
-// Check if jQuery's loaded
-function GM_wait() {
-  if (typeof unsafeWindow.jQuery == 'undefined') {
-    window.setTimeout(GM_wait, 100);
+original = $this.html();
+regex = new RegExp("\\(([^\"/]+) AutoUnRAR\\)","ig");
+$this.html($this.html().replace(regex, ") (<font class='filename'>$1</span> AutoUnRAR)"));
+if ($this.html() == original) {
+  regex = new RegExp("\"(.+)\".+yEnc","ig");
+  $this.html($this.html().replace(regex, "\"<font class='filename'>$1</span>\" yEnc"));
+}
+});
+$("font.filename").css({"font-weight": "bold", "background-color": "LightYellow"});
+//Have clicking the big thumb toggle the associated checkbox
+$("a[target='thumblarge2'] > img").each(function() {
+$anchor = $(this).parent();
+var thumbid = $anchor.siblings("input")[0].id;
+$anchor.attr({"href": "", "target": ""});
+$anchor.click(function(event) {
+  event.preventDefault();
+  if (document.getElementById(thumbid).checked) {
+    document.getElementById(thumbid).checked = false;
+    $(event.target).parent().parent().parent().css("background-color", "#E5E5E5")
   } else {
-    $ = unsafeWindow.jQuery.noConflict(true);
-    letsJQuery();
+    document.getElementById(thumbid).checked = true;
+    $(event.target).parent().parent().parent().css("background-color", "Gold")
   }
-}
-*/
-function letsJQuery() {
-  var thisElement, regex, original;
-
-  $("a[target^='thumb']").css({"color": "DarkGreen", "background-color": "AliceBlue"});
-  $("a[target^='nfo']").css({"color": "DarkRed", "background-color": "AliceBlue"});
-  $("a[target^='par']").css({"color": "DarkBlue", "background-color": "AliceBlue"});
-  $("a[target^='nzb']").css({"color": "DarkSlateBlue", "background-color": "AliceBlue"});
-  $("a[target^='subj']").each(function() {
-    var $this = $(this);
-    regex = new RegExp("<br>","ig");
-    $this.html($this.html().replace(regex, " "));
-
-    original = $this.html();
-    regex = new RegExp("\\(([^\"/]+) AutoUnRAR\\)","ig");
-    $this.html($this.html().replace(regex, ") (<font class='filename'>$1</span> AutoUnRAR)"));
-    if ($this.html() == original) {
-      regex = new RegExp("\"(.+)\".+yEnc","ig");
-      $this.html($this.html().replace(regex, "\"<font class='filename'>$1</span>\" yEnc"));
-    }
-  });
-  $("font.filename").css({"font-weight": "bold", "background-color": "LightYellow"});
-  //Have clicking the big thumb toggle the associated checkbox
-  $("a[target='thumblarge2'] > img").each(function() {
-    $anchor = $(this).parent();
-    var thumbid = $anchor.siblings("input")[0].id;
-    $anchor.attr({"href": "", "target": ""});
-    $anchor.click(function(event) {
-      event.preventDefault();
-      if (document.getElementById(thumbid).checked) {
-        document.getElementById(thumbid).checked = false;
-        $(event.target).parent().parent().parent().css("background-color", "#E5E5E5")
-      } else {
-        document.getElementById(thumbid).checked = true;
-        $(event.target).parent().parent().parent().css("background-color", "Gold")
-      }
-    });
-  });
-}
-letsJQuery();
+});
+});
